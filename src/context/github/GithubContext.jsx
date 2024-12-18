e01784e5d9b85b5b31880fe20f3313db000d997e
Aqil321821 <aqilrao321821@gmail.com>
2024-12-18 23:50:07 +0500

    work done untill alert context and reducer

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -12,25 +12,27 @@ export const GithubProvider = ({ children }) => {
   };
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  const fetchUsers = async () => {
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`, {
+
+    const response = await fetch(`${GITHUB_URL}/search/users?q=${text}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
-    const data = await response.json();
-    console.log(data);
+    const { items } = await response.json();
 
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     });
   };
+
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
 
   //set loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
-  return <GithubContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers }}>{children}</GithubContext.Provider>;
+  return <GithubContext.Provider value={{ users: state.users, loading: state.loading, searchUsers, clearUsers }}>{children}</GithubContext.Provider>;
 };
 export default GithubContext;
